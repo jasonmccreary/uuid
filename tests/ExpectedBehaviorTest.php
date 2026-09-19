cd4c049712c372828f610c3142d40ad825602b21
Jason McCreary <jason@pureconcepts.net>
2026-09-19 17:35:38 -0400

    Drop stub for undeclared method

--- tests/ExpectedBehaviorTest.php
+++ tests/ExpectedBehaviorTest.php
@@ -557,10 +557,6 @@ class ExpectedBehaviorTest extends TestCase
             });
 
         $timeProvider = Double::for('Ramsey\Uuid\Provider\TimeProviderInterface');
-        $timeProvider->allows('currentTime')->returns([
-                'sec' => 1578522046,
-                'usec' => 10000,
-            ]);
         $timeProvider->allows('getTime')->returns(new Time(1578522046, 10000));
 
         $generator = new DefaultTimeGenerator($nodeProvider, $timeConverter, $timeProvider);
